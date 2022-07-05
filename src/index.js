0a08d1f8e15de8c4f36f8143c05c8e2e43ce299b
Vipin <2002vipin318@gmail.com>
2022-07-05 16:12:26 +0530

    Add Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store =legacy_createStore(movies);
+console.log('store',store);
+console.log('store',store.getState());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
