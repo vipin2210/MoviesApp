fab9f852c8ece1264551332be7c71730f9348978
Vipin <2002vipin318@gmail.com>
2022-10-13 17:15:07 +0530

    Deploy React app to Github Pages

---
+++ precache-manifest.0b4ba20feb36fa3417b59e689f777ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece1d159b64d2902e8ff",
+    "url": "/MoviesApp/static/css/main.1a18dffd.chunk.css"
+  },
+  {
+    "revision": "ece1d159b64d2902e8ff",
+    "url": "/MoviesApp/static/js/main.ece1d159.chunk.js"
+  },
+  {
+    "revision": "abfba63e22d1c5adbf43",
+    "url": "/MoviesApp/static/js/1.abfba63e.chunk.js"
+  },
+  {
+    "revision": "73b0a5866496ff89dc3e",
+    "url": "/MoviesApp/static/js/runtime~main.73b0a586.js"
+  },
+  {
+    "revision": "50a5eb76527e5c567060b2a8cc309990",
+    "url": "/MoviesApp/index.html"
+  }
+];
